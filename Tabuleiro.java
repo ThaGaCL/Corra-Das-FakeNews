629039bc635d28682b9f1e62f6a2dc6fad67ce08
Thilons <tgcl21@inf.ufpr.br>
2023-05-07 15:42:10 -0300

    Inicial

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -1,13 +1,14 @@
-import java.util.Scanner;
-
 public class Tabuleiro
 {
 
     int tamanho;
 
+    public Tabuleiro(){
+        this.tamanho = 9;
+    }
 
     public void desenhaTabuleiro(){
-        System.out.println(Cores.ANSI_WHITE + "+---+---+---+---+---+---+---+---+---+" * Cores.ANSI_RESET);
+        System.out.println(Cores.ANSI_WHITE + "+---+---+---+---+---+---+---+---+---+" + Cores.ANSI_RESET);
 
     }
 }
