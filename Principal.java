c09b586ed545185b26ede7518bb8cb3ab2d676eb
Thilons <tgcl21@inf.ufpr.br>
2023-05-07 15:37:42 -0300

    Iniciando impressão do tabuleiro

---
+++ Principal.java
@@ -0,0 +1,8 @@
+public class Principal
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tabuleiro tabuleiro = new Tabuleiro(tamanho);\
+        tabuleiro.desenhaTabuleiro();
+    }
+}
---
+++ Tabuleiro.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Tabuleiro
+{
+
+    int tamanho;
+
+
+    public void desenhaTabuleiro(){
+        System.out.println(Cores.ANSI_WHITE + "+---+---+---+---+---+---+---+---+---+" * Cores.ANSI_RESET);
+
+    }
+}
