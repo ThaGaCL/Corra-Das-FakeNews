3789ffce06414dedc74241c73c03873a441e844f
Fábio Naconeczny da Silva <113272497+FabioKekw@users.noreply.github.com>
2023-06-13 15:00:45 -0300

    Delete Project2 directory

--- Project2/Movimento.java
+++
@@ -1,9 +0,0 @@
-public interface Movimento {
-
-    public abstract void movimentar(int movimento);
-    public void movimentarNorte();
-    public void movimentarSul();
-    public void movimentarLeste();
-    public void movimentarOeste();
-
-}
